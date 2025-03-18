a434a83151c8aa4b3939bf449a78657959b3a110
Amir <salehamir708@gmail.com>
2025-03-18 11:56:49 +0330

    Images and initial-gui f. are added

--- GUI/__app__.py
+++ GUI/__app__.py
@@ -1,6 +1,16 @@
 '''
 This file contains GUI codes
 '''
+import customtkinter
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
 
-import customtkinter as ctk
+        self.title("Managia")
+        self.geometry("800x600")
+        self.grid_columnconfigure((0,1), weight=1)
+        self.grid_rowconfigure((0, 1), weight=1)
+        
 
+app = App()
+app.mainloop()
